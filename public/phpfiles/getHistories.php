4b68a545f9040c5199767f2a2a4e57200a616a02
boazzzzz <boaz0105@gmail.com>
2022-12-28 17:07:02 +0800

    update

--- public/phpfiles/getHistories.php
+++ public/phpfiles/getHistories.php
@@ -3,9 +3,9 @@ header('Access-Control-Allow-Origin:*');
 header("Content-Type:application/json;charset=utf-8");
 
 require_once("./php_connect_books/connectBooks.php");
-$sql = "SELECT s.story_id, a.story_article_id, s.story_name, s.story_risk, s.story_spot, s.story_specialty, s.story_cover, a.story_title, a.story_img, a.story_content 
-        FROM story s JOIN story_article a ON s.story_id = a.story_id
-        ORDER BY a.story_article_id;";
+$sql = "SELECT story_id, story_classification, story_name, story_risk, story_intro, story_specialty, story_spot, story_age, itinerary_number_of_years
+        FROM story
+        ORDER BY story_id;";
 $histories = $pdo->query($sql);
 $storyRows = $histories->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($storyRows);
--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -157,16 +157,19 @@
       stripe
       border
       :columns="columns"
-      :data="data"
+      :data="dataList"
       width="1000px"
    >
       <!-- 加入開關按鈕 -->
-      <template #on_off>
+      <template #on_off="{ row }">
          <Switch
             size="large"
             true-color="#fab042"
             false-color="#e6e6e6"
-            v-model="story_status"
+            v-model="row.story_status"
+            :true-value="parseInt(1)"
+            :false-value="parseInt(0)"
+            @on-change="onChange(row)"
          >
             <template #open>
                <span>上架</span>
@@ -182,12 +185,12 @@
       </template>
 
       <!-- 加入編輯、刪除彈窗 -->
-      <template #edit_del>
+      <template #edit_del="{ index, row }">
          <!-- 編輯按鈕 -->
-         <Button @click="modal3 = true" class="edit">編輯</Button>
+         <Button @click="clickEditBtn(index, row)" class="edit">編輯</Button>
          <!-- 編輯彈窗 -->
          <Modal
-            v-model="modal3"
+            v-model="modal3[index]"
             title="編輯歷史故事"
             ok-text="確認修改"
             cancel-text="取消"
@@ -195,6 +198,85 @@
             class="editnews-popup"
             :styles="{ top: '30px' }"
          >
+            <Form :model="addItem" :label-width="80" :rules="ruleInline" inline>
+               <FormItem label="故事編號">
+                  <Input
+                     v-model="addItem.story_id"
+                     placeholder="請輸入故事編號"
+                  ></Input>
+               </FormItem>
+               <FormItem label="故事分類" prop="story_classification">
+                  <Select
+                     v-model="addItem.story_classification"
+                     placeholder="請選擇"
+                  >
+                     <Option value="經典行程">經典行程</Option>
+                     <Option value="期間限定">期間限定</Option>
+                  </Select>
+               </FormItem>
+            </Form>
+            <Form :model="addItem" :label-width="80">
+               <FormItem label="故事名稱">
+                  <Input
+                     v-model="addItem.story_name"
+                     placeholder="請輸入故事標題"
+                  ></Input>
+               </FormItem>
+            </Form>
+            <Form :model="addItem" :label-width="80" :rules="ruleInline" inline>
+               <FormItem label="故事年代">
+                  <Input
+                     v-model="addItem.story_age"
+                     placeholder="請輸入故事年代"
+                  ></Input>
+               </FormItem>
+               <FormItem label="穿越日期">
+                  <Input
+                     v-model="addItem.itinerary_number_of_years"
+                     placeholder="請輸入確切穿越日期"
+                  ></Input>
+               </FormItem>
+            </Form>
+            <Form :model="addItem" :label-width="80" :rules="ruleInline" inline>
+               <FormItem label="危險度">
+                  <Select
+                     v-model="addItem.story_risk"
+                     placeholder="請選擇危險度"
+                  >
+                     <Option value="低">低</Option>
+                     <Option value="中">中</Option>
+                     <Option value="高">高</Option>
+                  </Select>
+               </FormItem>
+               <FormItem label="特色">
+                  <Select
+                     v-model="addItem.story_specialty"
+                     placeholder="請選擇特色"
+                  >
+                     <Option value="奇聞軼事">奇聞軼事</Option>
+                     <Option value="歷史事件">歷史事件</Option>
+                     <Option value="宗教事件">宗教事件</Option>
+                  </Select>
+               </FormItem>
+               <FormItem label="地區">
+                  <Select v-model="addItem.story_spot" placeholder="請選擇地區">
+                     <Option value="歐洲">歐洲</Option>
+                     <Option value="北美洲">北美洲</Option>
+                     <Option value="南美洲">南美洲</Option>
+                     <Option value="非洲">非洲</Option>
+                     <Option value="亞洲">亞洲</Option>
+                     <Option value="大洋洲">大洋洲</Option>
+                  </Select>
+               </FormItem>
+               <FormItem label="故事簡介">
+                  <Input
+                     v-model="addItem.story_intro"
+                     type="textarea"
+                     style="width: 100%"
+                     :autosize="{ minRows: 10, maxRows: 50 }"
+                  ></Input>
+               </FormItem>
+            </Form>
             <Form :model="addItem" :label-width="80">
                <FormItem label="故事大圖">
                   <input
@@ -205,8 +287,13 @@
                   />
                </FormItem>
                <FormItem label="段落一" style="font-weight: 700"> </FormItem>
-               <FormItem label="故事圖片">
-                  <input type="file" multiple />
+               <FormItem label="故事圖片1">
+                  <input
+                     id="story_img01_id"
+                     type="file"
+                     ref="img01Input"
+                     multiple
+                  />
                </FormItem>
                <FormItem label="故事內容">
                   <Input
@@ -216,8 +303,13 @@
                   ></Input>
                </FormItem>
                <FormItem label="段落二" style="font-weight: 700"> </FormItem>
-               <FormItem label="故事圖片">
-                  <input type="file" multiple />
+               <FormItem label="故事圖片2">
+                  <input
+                     id="story_img02_id"
+                     type="file"
+                     ref="img02Input"
+                     multiple
+                  />
                </FormItem>
                <FormItem label="故事內容">
                   <Input
@@ -227,8 +319,13 @@
                   ></Input>
                </FormItem>
                <FormItem label="段落三" style="font-weight: 700"> </FormItem>
-               <FormItem label="故事圖片">
-                  <input type="file" multiple />
+               <FormItem label="故事圖片3">
+                  <input
+                     id="story_img03_id"
+                     type="file"
+                     ref="img03Input"
+                     multiple
+                  />
                </FormItem>
                <FormItem label="故事內容">
                   <Input
@@ -238,8 +335,13 @@
                   ></Input>
                </FormItem>
                <FormItem label="段落四" style="font-weight: 700"> </FormItem>
-               <FormItem label="故事圖片">
-                  <input type="file" multiple />
+               <FormItem label="故事圖片4">
+                  <input
+                     id="story_img04_id"
+                     type="file"
+                     ref="img04Input"
+                     multiple
+                  />
                </FormItem>
                <FormItem label="故事內容">
                   <Input
@@ -249,8 +351,13 @@
                   ></Input>
                </FormItem>
                <FormItem label="段落五" style="font-weight: 700"> </FormItem>
-               <FormItem label="故事圖片">
-                  <input type="file" multiple />
+               <FormItem label="故事圖片5">
+                  <input
+                     id="story_img05_id"
+                     type="file"
+                     ref="img05Input"
+                     multiple
+                  />
                </FormItem>
                <FormItem label="故事內容">
                   <Input
@@ -263,7 +370,7 @@
          </Modal>
 
          <!-- 刪除按鈕 -->
-         <Button class="delete" @click="remove(data.id)">刪除</Button>
+         <Button class="delete" @click="remove(index, row)">刪除</Button>
       </template>
    </Table>
 </template>
@@ -275,7 +382,7 @@ export default {
    data() {
       return {
          modal1: false, //新增彈窗預設關閉
-         modal3: false, //編輯彈窗預設關閉
+         modal3: [], //編輯彈窗預設關閉
          columns: [
             ///表單表頭
             {
@@ -353,33 +460,7 @@ export default {
                slot: "edit_del", //加入編輯刪除欄位需加slot
             },
          ],
-         data: [
-            ///表格內容資料
-            {
-               story_id: "1001",
-               story_name: "史前時代",
-               story_classification: "經典行程",
-               story_risk: "低",
-               story_specialty: "宗教事件",
-               story_spot: "北美洲",
-            },
-            {
-               story_id: "1002",
-               story_name: "哥倫布發現新大陸",
-               story_classification: "經典行程",
-               story_risk: "中",
-               story_specialty: "奇聞軼事",
-               story_spot: "歐洲",
-            },
-            {
-               story_id: "2001",
-               story_name: "鐵達尼號沈船事故",
-               story_classification: "期間限定",
-               story_risk: "高",
-               story_specialty: "歷史事件",
-               story_spot: "歐洲",
-            },
-         ],
+         dataList: [],
          addItem: {
             //新增彈窗內容資料
             story_id: "",
@@ -401,33 +482,84 @@ export default {
             story_content_03: "",
             story_content_04: "",
             story_content_05: "",
-            story_status: "",
+            story_status: 0,
          },
-         editItem: {
-            id: "addItem.story_id()",
-            title: "",
-            select: "",
-            date: "",
-            textarea: "",
-         },
+         // editItem: {
+         //    // id: "addItem.story_id()",
+         //    title: "",
+         //    select: "",
+         //    date: "",
+         //    textarea: "",
+         // },
       };
    },
-   created() {
-      fetch(`${BASE_URL}/getHistories.php`);
+   mounted() {
+      this.getData();
    },
    methods: {
-      remove() {
+      remove(index, row) {
          this.$Modal.confirm({
             content: "<p>確認刪除嗎?</p>",
+            okText: "確定",
+            cancelText: "再看一下",
             onOk: () => {
                this.$Message.info("確認刪除");
-               this.data.splice("id", 1);
+               this.deleData(row);
+               this.dataList.splice(index, 1);
             },
             onCancel: () => {
                this.$Message.info("取消");
             },
          });
       },
+      getData() {
+         fetch(`${BASE_URL}/getHistories.php`)
+            .then((res) => res.json())
+            .then((result) => {
+               this.dataList = result;
+               console.log(this.dataList);
+            });
+      },
+      onChange(row) {
+         if (row.story_status) {
+            this.$Message.info("狀態： 上架");
+         } else {
+            this.$Message.info("狀態： 下架");
+         }
+         // console.log(row);
+      },
+      clickEditBtn(index, row) {
+         this.modal3[index] = true;
+         const story_id = row.story_id;
+         // console.log(row.story_id);
+
+         fetch(`${BASE_URL}/getHistoryArticles.php`, {
+            method: "POST",
+            body: JSON.stringify({
+               story_id: story_id,
+            }),
+         })
+            .then((res) => res.json())
+            .then((result) => {
+               // console.log(result);
+               const articles = {
+                  story_title_01: result[0].story_title,
+                  story_title_02: result[1].story_title,
+                  story_title_03: result[2].story_title,
+                  story_title_04: result[3].story_title,
+                  story_title_05: result[4].story_title,
+                  story_content_01: result[0].story_content,
+                  story_content_02: result[1].story_content,
+                  story_content_03: result[2].story_content,
+                  story_content_04: result[3].story_content,
+                  story_content_05: result[4].story_content,
+               };
+               // console.log(articles);
+               this.addItem = { ...this.dataList[index], ...articles };
+               // console.log(this.addItem);
+            });
+      },
+      deleData(row) {},
    },
 };
 </script>
