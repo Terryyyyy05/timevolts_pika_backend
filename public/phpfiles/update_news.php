248673b0c4a07404f3da5e1ef2a253540d0e46c0
irene0103 <miki1234594@gmail.com>
2022-12-29 03:14:11 +0800

    try try

--- public/phpfiles/update_news.php
+++ public/phpfiles/update_news.php
@@ -2,9 +2,6 @@
 header('Access-Control-Allow-Origin:*');
 header("Content-Type:application/json;charset=utf-8");
 
-//線上
-// const MY_DIR = "../../../timevolts_pika/news";
-//local
 require_once("./img_path.php");
 const MY_DIR = img_path;
 
@@ -41,13 +38,32 @@ switch($_FILES["news_img"]["error"] ){
     default:
         $msg = "上傳檔案失敗，錯誤代碼: ".$_FILES["error"]."請通知系統開發人員";
 }
-
 if( $_FILES["news_img"]["error"] === 0){
   	
 	try{
-		require_once("./php_connect_books/connectBooks.php");
+		require_once("./connectBooks.php");
+
 		//sql 指令
-		$sql = "insert into news values (:news_id, :news_title, :news_img, :news_content, :news_add_date, :news_status, :news_item_id, :news_category, :news_hashtag)";
+    $checkSql = "SELECT `news_id` FROM `news` where `news_title` = '{$_POST["pro_title"]}'";
+    $products = $pdo->query($checkSql);
+    $prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
+    
+    if($products->rowCount() > 0 ){
+      echo json_encode('wrong');
+      exit();
+    }
+    
+		$sql = "update news set 
+							new_id=:news_id, 			
+							news_title=:news_title, 			
+							news_img=:news_img, 		
+							news_content=:news_content, 
+							news_add_date=:news_add_date, 
+							news_status=:news_status, 
+							news_item_id=:news_item_id, 
+							news_category=:news_category, 
+							news_hashtag=:news_hashtag
+                            where news_id = :news_id";
 		//編譯, 執行
 		$products = $pdo->prepare($sql);	
 		$products->bindValue(":news_id", $_POST["news_id"]);
@@ -61,7 +77,9 @@ if( $_FILES["news_img"]["error"] === 0){
 		$products->bindValue(":news_hashtag", $_POST["news_hashtag"]);
 		$products->execute();	
 
-	    $msg = "新增成功";
+    
+    $msg = "更新成功";
+    
 	} catch (PDOException $e) {
 		$msg = "錯誤行號 : ".$e->getLine().", 錯誤訊息 : ".$e->getMessage();
 	}	
